3c0cf0fdaca66e099defb92bc12810d07a7163a9
Faraz Najam <faraz_najam@windowslive.com>
2025-08-09 07:37:05 +1000

    Your commit message describing the changes

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ module "key_vault" {
 
 module "sql" {
   source              = "./modules/sql"
-  name                = var.name
+  name                = var.sqldb_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
   sql_admin_username  = var.sql_admin_username
