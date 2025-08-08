7135e6e8b5a83957c3e5dea82e702336bfa7e9ba
Faraz Najam <faraz_najam@windowslive.com>
2025-08-09 06:58:57 +1000

    Your commit message describing the changes

--- modules/sql/main.tf
+++ modules/sql/main.tf
@@ -21,10 +21,3 @@ output "sql_server_name" {
   value = azurerm_mssql_server.sqlserver.name
 }
 
-output "sql_database_name" {
-  value = azurerm_mssql_database.sqldb.name
-}
-
-output "sql_server_fqdn" {
-  value = azurerm_mssql_server.sqlserver.fully_qualified_domain_name
-}
