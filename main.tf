d2467e408821470904de74a1ab0b49f3aa506120
Faraz Najam <faraz_najam@windowslive.com>
2025-08-09 19:38:37 +1000

    Your commit message describing the changes

--- main.tf
+++ main.tf
@@ -52,12 +52,20 @@ module "sql" {
   sqlserver_name      = var.sqlserver_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
-  sql_admin_username  = var.sql_admin_username
-  sql_admin_password  = var.sql_admin_password
+  sql_admin_username  = data.azurerm_key_vault_secret.sql_admin_username.value
+  sql_admin_password  = data.azurerm_key_vault_secret.sql_admin_password.value
   sql_sku_name        = var.sql_sku_name
   max_size_gb         = 10
   zone_redundant      = false
   read_scale          = false
 }
 
+data "azurerm_key_vault_secret" "sql_admin_password" {
+  name         = "SqlAdminPassword"   # the name you gave the secret in Key Vault
+  key_vault_id = module.key_vault.key_vault_id
+}
 
+data "azurerm_key_vault_secret" "sql_admin_username" {
+  name         = "SqlAdminUsername"
+  key_vault_id = module.key_vault.key_vault_id
+}
