a133dbaee8e41fabf6cc540f51d11616be296e39
Faraz Najam <faraz_najam@windowslive.com>
2025-08-11 01:11:31 +1000

    Your commit message describing the changes

--- modules/app_service/variables.tf
+++ modules/app_service/variables.tf
@@ -1,23 +1,3 @@
-variable "app_service_name" {
-  type = string
-}
-
-variable "location" {
-  type = string
-}
-
-variable "resource_group" {
-  type = string
-}
-
-variable "app_settings" {
-  type    = string
-  default = https://azkeyvaultrg8531.vault.azure.net/
-}
-
-variable "key_vault_uri" {
-  type = string
-}
 
 variable "app_service_name" {
   type = string
